3c746b3e2b93d891b54316e5b32d26619daa8624
Lukasz Szczupak <szczupak.lukasz@o2.pl>
2020-02-24 13:29:34 +0100

    Przesunięcie tablicy

--- src/MoveArray.java
+++ src/MoveArray.java
@@ -1,16 +1,15 @@
-public class ArrayMove {
+public class MoveArray {
     public static void main(String[] args) {
 
         int[] array = {1, 2, 3, 4, 5};
-        int[] array1 = {5, 6, 7, 8, 9};
         int[] moveArray = moveArray(array, 3);
+        int[] array1 = {5, 6, 7, 8, 9};
         int[] moveArray1 = moveArray(array1, 4);
         System.out.println(showArray(array));
         System.out.println(showArray(moveArray));
         System.out.println(showArray1(array1));
         System.out.println(showArray1(moveArray1));
     }
-
     public static int[] moveArray(int[] array, int N) {
 
         if (array == null) {
@@ -30,16 +29,13 @@ public class ArrayMove {
             return moveArray;
         }
     }
-
     public static String showArray(int[] array) {
         return "Tablica nr.1: " + "{" + array[0] + ", " + array[1] +
                 ", " + array[2] + ", " + array[3] + ", " + array[4] + "}";
     }
-
     public static String showArray1(int[] array2) {
         return "Tablica nr.2: " + "{" + array2[0] + ", " + array2[1] +
                 ", " + array2[2] + ", " + array2[3] + ", " + array2[4] + "}";
     }
-
 }
 
